4b496c9c52091659ea3d1fd1f6e9f03e70673c21
Furkan9623 <irctcticket81@gmail.com>
2023-01-06 21:07:11 +0530

    click event on node js

--- Event Module #20/index.js
+++ Event Module #20/index.js
@@ -13,3 +13,14 @@ event.on("Click", () => {
 });
 
 event.emit("Click"); // emit nothing but we can say its is addEvent lisner we use in java script, this emit we not call before the event.on method
+
+// ❓ Registering for event with callback parameter
+
+const clickEvent = require("events");
+const eventData = new clickEvent();
+
+eventData.on("passParameter", (statusCode, message) => {
+  console.log(`Status code is ${statusCode} and response is ${message}`);
+});
+
+eventData.emit("passParameter", 200, "ok");
